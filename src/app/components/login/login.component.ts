788950f56330b5f3f38243f2c94f45109482b7b8
Sharan Reddy <dropmailtosharan1@gmail.com>
2025-03-14 22:17:56 +0530

    form application with validations updated

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,13 +1,76 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, ReactiveFormsModule, FormsModule, CommonModule],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  registrationForm: FormGroup;
+  formBuilder = inject(FormBuilder);
+  submitted = false;
+  showPassword =  false;
 
+  constructor() {
+    this.registrationForm = this.formBuilder.group(
+      {
+        firstName: ['', [Validators.required]],
+        lastName: ['', [Validators.required]],
+        email: ['', [Validators.required, Validators.email]],
+        password: ['', [Validators.required, Validators.minLength(8)]],
+        confirmPassword: ['', [Validators.required]],
+        acceptTerms: [false, Validators.requiredTrue]
+      },
+      {
+        validators: this.passwordMatchValidation,
+      }
+    );
+  }
+
+  ngOnInit(): void {}
+
+  passwordMatchValidation(form: FormGroup) {
+    const password = form.get('password');
+    const confirmPassword = form.get('confirmPassword');
+    if (
+      password &&
+      confirmPassword &&
+      password.value !== confirmPassword.value
+    ) {
+      return {
+        mismatch: true,
+      };
+    } else {
+      return null;
+    }
+  }
+
+  toggleShowPassword() {
+    this.showPassword = !this.showPassword;
+  }
+
+  onCheckboxChange() {
+    this.registrationForm.get('acceptTerms')?.updateValueAndValidity(); // ✅ Update validation state on change
+  }
+
+  onSubmit() {
+    console.log('Form Submitted:', this.registrationForm.value);
+    this.submitted = true;
+  }
+
+  reset() {
+    this.submitted = false;
+    this.registrationForm.reset();
+  }
 }
